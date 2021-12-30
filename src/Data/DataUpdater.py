6654c4cca0653261da6c45bbd72f920baa12a13f
Kyle Blue <kyle.blue.nuttall@gmail.com>
2021-12-30 14:33:59 +0000

    Add tokens file

--- src/Data/DataUpdater.py
+++ src/Data/DataUpdater.py
@@ -1,2 +1,10 @@
+"""
+Check data in local directory -> is it sufficient?
+If not, check to see if required data is in Google Drive. Get what you can.
+Need more data? Use polygon.io API
+
+Once you have all data, save it locally
+"""
+
 class DataUpdater:
     pass
