6591f9401cbae73694085a71acaaa818f86435db
Kyle Doidge <kyle.blue.nuttall@gmail.com>
2022-02-04 22:35:39 +0000

    Add install instructions in Readme

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ Generates models and uploads the best to the model repo
 
 ## Data
 Data is saved to google drive and locally. Data is initially obtained using the Polygon.io API. It is saved to Google drive in a CSV format
+
+## Prerequisites
+
+- Have python 3.9 installed
+- Have VS Build tools 2014+ and Visual C++ build tools (with windows sdk)
+- If using Windows, do not run this in WSL, you may run into GPU issues
+
+### Install in this order:
+
+- CUDA toolkit: https://developer.nvidia.com/cuda-toolkit-archive
+- CudNN: https://developer.nvidia.com/cudnn (install instructions: https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html#install-windows)
+- (optional but improves gpu latency) TensorRT: https://developer.nvidia.com/tensorrt-getting-started (install instructions: https://docs.nvidia.com/deeplearning/tensorrt/install-guide/index.html#installing-zip)
+
+More up to date instructions may live here: https://www.tensorflow.org/install/gpu
--- src/Data/TSDataPreprocessor.py
+++ src/Data/TSDataPreprocessor.py
@@ -1,3 +1,4 @@
+from typing import Dict, Callable
 import pandas as pd
 
 class TSDataPreprocessor():
@@ -5,20 +6,26 @@ class TSDataPreprocessor():
     Time-Series Data Preprocessor (for use with RNN)
     """
     def __init__(self, raw_data: pd.DataFrame, *,
-        forecast_col_name: str,
+        target_col_name: str,
         sequence_length = 100,
-        forecast_period = 10
+        forecast_period = 10,
+        custom_norm_functions: Dict[str, Callable[[pd.Series], pd.Series]] = {},
     ):
         """
         If preprocessed data already exists in the root data folder, it will be loaded, and preprocessing will be skipped
         We will check if it already exists by obtaining a hash of the raw_data dataframe, and comparing it to the hash saved in
         the name of the file. This will mean the same raw data was once passed before.
+
+        @param: custom_norm_functions : a dictionary of column names and their associated normalisation function. Any column
+        not specified uses the default normalisation function, which is percentage change, then standardisation.
+        
         """
         self.raw_data = raw_data
         self.df = raw_data.copy()
-        self.forecast_col_name = forecast_col_name
+        self.target_col_name = target_col_name
         self.sequence_length = sequence_length
         self.forecast_period = forecast_period
+        self.custom_norm_functions = custom_norm_functions
         data_hash = hex(hash(raw_data.to_numpy().tobytes()))[2:8]
 
     
@@ -41,8 +48,18 @@ class TSDataPreprocessor():
 
 
         # Convert to pct change
-
+        keys = self.custom_norm_functions.keys()
+        for col_name in self.df:
+            new_col
+            if col_name not in keys:
+                new_col = self.default_norm(self.df[col_name])
+            else:
+                new_col = self.custom_norm_functions[col_name](self.df[col_name])
+            self.df[col_name] = new_col
+    
         # Add Target
+
+        self.df[self.target_col_name] = target_col
 
         # Standardise / Normalise (maybe pass these functions in?)
 
