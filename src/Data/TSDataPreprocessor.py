Fix normalisation. Allow saving model as a tarball in temp dir

src/Data/TSDataPreprocessor.py
@@ -20,8 +20,10 @@ def minmax_norm(array: Union[list, np.ndarray]) -> pd.Series:
     np_array = np.array(array)
     return pd.Series((np_array - np.min(np_array)) / (np.max(array) - np.min(array)))
 
-def standardise(arr: np.ndarray):
-    return (arr - np.mean(arr)) / np.std(arr)
+def standardise(arr: np.ndarray, mean: Optional[float] = None, std: Optional[float] = None):
+    mean = mean or np.mean(arr)
+    std = std or np.std(arr)
+    return (arr - mean) / std
 
 @dataclass
 class PreprocessedFileInfo:
@@ -218,23 +220,27 @@ class TSDataPreprocessor():
 
 
     @staticmethod
-    def std_normalisation(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
+    def std_normalisation(df: pd.DataFrame, col_name: str, col_config: ColumnConfig) -> pd.DataFrame:
         df[col_name] = df[col_name].pct_change()
         df.dropna(inplace=True)
-        df[col_name] = standardise(df[col_name].to_numpy())
+        np_data = df[col_name].to_numpy()
+        mean = np.mean(np_data)
+        std = np.std(np_data)
+        col_config.add_args(col_name, {"mean": mean, "std": std})
+        df[col_name] = standardise(np_data, mean, std)
         df.dropna(inplace=True)
         return df
 
     @staticmethod
-    def ma_std_normalisation(df: pd.DataFrame, col_name: str, period: int) -> pd.DataFrame:
+    def ma_std_normalisation(df: pd.DataFrame, col_name: str, period: int, col_config: ColumnConfig) -> pd.DataFrame:
         df[col_name] = df[col_name].rolling(period, center=False).mean()
-        print(df[col_name])
         df[col_name] = df[col_name].pct_change()
         df.dropna(inplace=True)
-        print(df[col_name])
-        df[col_name] = standardise(df[col_name].to_numpy())
-        print(df[col_name])
-        print(df)
+        np_data = df[col_name].to_numpy()
+        mean = np.mean(np_data)
+        std = np.std(np_data)
+        col_config.add_args(col_name, {"mean": mean, "std": std})
+        df[col_name] = standardise(np_data, mean, std)
         df.dropna(inplace=True)
         
         return df
@@ -258,12 +264,11 @@ class TSDataPreprocessor():
     def normalisation(df: pd.DataFrame, col_config: ColumnConfig) -> pd.DataFrame:
         config = col_config.to_dict()
         for col_name, value in config.items():
-            lendf = len(df)
             if col_name == "target": continue
             if value["norm_function"] == NormFunction.STD:
-                df = TSDataPreprocessor.std_normalisation(df, col_name)
+                df = TSDataPreprocessor.std_normalisation(df, col_name, col_config)
             if value["norm_function"] == NormFunction.MA_STD:
-                df = TSDataPreprocessor.ma_std_normalisation(df, col_name, value["period"])
+                df = TSDataPreprocessor.ma_std_normalisation(df, col_name, value["period"], col_config)
             if value["norm_function"] == NormFunction.MINMAX:
                 df = TSDataPreprocessor.minmax_normalisation(df, col_name)
             if value["norm_function"] == NormFunction.TIME:
@@ -293,9 +298,10 @@ class TSDataPreprocessor():
         ## TODO: Reorder col_config to be in same order as raw_data
         ## TODO: col_config documentation
 
-        dataset = self.load_existing_dataset(raw_data)
-        if dataset is not None:
-            return dataset 
+        #TODO: THIS DOESN'T CURRENTLY WORK. MEANS AND STD AREN'T ADDED TO COL_CONFIG AMONG OTHER THINGS
+        # dataset = self.load_existing_dataset(raw_data)
+        # if dataset is not None:
+        #     return dataset 
 
         df = raw_data.copy()
         df_title = df.style.caption or "~"

src/Model/RNN.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 from datetime import date, datetime
+import json
 from msilib.schema import Error
 from platform import architecture
-from typing import List, Tuple, Union
+from typing import Dict, List, Tuple, Union
+
+from Data.ColumnConfig import ColumnConfig
 from .Model import Model
 from enum import Enum
 from keras.layers import LSTM, GRU, Dense, Input, Bidirectional, Dropout, BatchNormalization
@@ -11,6 +14,8 @@ from Data.TSDataPreprocessor import Dataset
 import keras
 import numpy as np
 import os
+import tarfile
+import tempfile
 
 @dataclass
 class ModelFileInfo:
@@ -21,6 +26,9 @@ class ModelFileInfo:
     timestamp: datetime
 
 ArchitectureType = Union[LSTM, GRU]
+def get_architecture_name(architecture_type: ArchitectureType):
+    if architecture_type == LSTM: return "LSTM"
+    if architecture_type == GRU: return "GRU"
 class RNN(Model):
 
     @staticmethod
@@ -52,6 +60,8 @@ class RNN(Model):
         self.dropout = dropout
         self.layers = layers
         self.model = self._create_model(x_shape, y_shape)
+        self.x_shape = x_shape
+        self.y_shape = y_shape
         self.score = {
             "loss": np.inf
         }
@@ -128,7 +138,7 @@ class RNN(Model):
         We use these files to init the model.
         """
 
-    def save_model(self, dataset: Dataset):
+    def save_model(self, col_config: ColumnConfig):
         """
         Save model locally
         
@@ -136,21 +146,35 @@ class RNN(Model):
         and date/time. Associated files will include metadata such as number of layers,
         each later shape, other stats etc. 
         """
-        if dataset.x_norm_functions is None:
-            raise Exception("dataset.x_norm_functions required to save model metadata")
-        dataset_means = []
-        dataset_stds = []
 
-        models_folder = os.path.join(os.environ["workspace"], "models")
-        os.makedirs(models_folder, exist_ok=True)
-        layer_text = "-".join([str(x) for x in self.layers])
-        timestamp = datetime.now().isoformat(timespec="seconds").replace(":", ";")
-        filename = f"RNN__{layer_text}__Loss-{self.score['loss']:.4f}__{timestamp}.h5"
-        self.model.save(os.path.join(models_folder, filename), save_format="h5")
-        self.model.summary()
-        for layer in self.model.layers:
-            print(layer.__class__.__name__)
-            print(layer.input_shape)
-            print(layer.output_shape)
-            if isinstance(layer, Dropout):
-                print(layer.get_config())
+        metadata: dict = {}
+        metadata["col_config"] = json.loads(col_config.to_json()) # I need a dict but in the json format
+        metadata["layers"] = self.layers
+        metadata["architecture"] = get_architecture_name(self.Architecture)
+        metadata["dropout"] = self.dropout
+        metadata["is_bidirectional"] = self.is_bidirectional
+        metadata["x_shape"] = self.x_shape
+        metadata["y_shape"] = self.y_shape
+        metadata_json = json.dumps(metadata, indent=2)
+        
+        with tempfile.TemporaryDirectory() as temp_dirname:
+            models_folder = os.path.join(os.environ["workspace"], "models")
+            os.makedirs(models_folder, exist_ok=True)
+            layer_text = "-".join([str(x) for x in self.layers])
+            timestamp = datetime.now().isoformat(timespec="seconds").replace(":", ";")
+            tar_filename = f"RNN__{layer_text}__Loss-{self.score['loss']:.4f}__{timestamp}.tar"
+
+            model_path = os.path.join(temp_dirname, "model.h5")
+            metadata_path = os.path.join(temp_dirname, "metadata.json")
+            self.model.save(model_path, save_format="h5")
+            with open(metadata_path, "w") as json_file:
+                json_file.write(metadata_json)
+
+            with tarfile.open(os.path.join(models_folder, tar_filename), "w") as tar:
+                tar.add(model_path, arcname="model.h5")
+                tar.add(metadata_path, arcname="metadata.json")
+        
+
+        
+
+        